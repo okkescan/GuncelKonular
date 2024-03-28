5782428e7742b23cbc97827b351b85b0bebfa736
Suleyman Abbud <suleymanabbud0@gmail.com>
2024-03-29 01:31:45 +0300

    Added Project Files

--- CaosApp/CaosApp/ClientApp/src/setupProxy.js
+++ CaosApp/CaosApp/ClientApp/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const { env } = require('process');
 
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:6994';
+  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:22079';
 
 const context = [
   "/weatherforecast",
