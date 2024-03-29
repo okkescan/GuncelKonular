673018170920e0d69c31363bf99c716e5c5d1a2e
Suleyman Abbud <suleymanabbud0@gmail.com>
2024-03-29 03:06:38 +0300

    added Project's Files

---
+++ CaosA/CaosA/ClientApp/src/components/Home.js
@@ -0,0 +1,14 @@
+﻿import React, { Component } from 'react';
+
+export class Home extends Component {
+  static displayName = Home.name;
+
+  render() {
+    return (
+      <div>
+            <h1>Yazılım Mühendisliği Güncel Konular Ders Projesi</h1>
+            <h2>ChaosMezuns</h2>
+      </div>
+    );
+  }
+}
